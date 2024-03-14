1e20f2c7e1fbbc303f3e3e078fbc33ce692c4c10
eesoymilk <qiyoudaoyi@gmail.com>
2024-03-14 16:38:09 +0800

    Init: HW1

--- code_practice/Simulation/kinematic.py
+++
@@ -1,9 +0,0 @@
-import abc
-import sys
-sys.path.append("..")
-from Simulation.utils import State, ControlState
-
-class KinematicModel:
-    @abc.abstractmethod
-    def step(self, state:State, cstate:ControlState) -> State:
-        return NotImplementedError
